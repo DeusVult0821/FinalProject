d1deb0727da517a81c4b65875838eec31ce61d8a
Erik <erikarancibia2108@gmail.com>
2022-05-23 23:31:08 +0600

    Fourth Fourth Commit

--- Project_Code/GUI_porject.py
+++ Project_Code/GUI_porject.py
@@ -7,6 +7,12 @@ root.title("Text Editor")
 root.iconbitmap("C:/Users/Erik/Desktop/GUI_Final_Project/Icon/Logo.ico")
 root.geometry("1200x800")
 
+global open_status_name
+open_status_name = False
+
+global selected
+selected = False
+
 #New File Function Creation
 def new_file():
 	#This delete the previous text
@@ -15,22 +21,37 @@ def new_file():
 	root.title("New Text")
 	status_bar.config(text="New File        ")
 
+	global open_status_name
+	open_status_name = False
+
 #Open File Function Creation
 def open_file():
 	#Delete text
 	The_text.delete("1.0", END)
+	
 	#Get filename
 	text_file = filedialog.askopenfilename(title="Open File", filetypes=(("Text File", "*.txt"), ("HTML File", "*.html"), ("Python File", "*.py"), ("All Files", "*.*")))
+	
+	#Check if there is a file name
+	if text_file:
+		#Makening sure that works
+		global open_status_name
+		open_status_name = text_file
+
 	#Update of the status bar
 	name = text_file
 	status_bar.config(text=f'{name}'        )
+	
 	#Open The File 
 	text_file = open(text_file, "r")
 	stuff = text_file.read()
+	
 	#Add file to the textbox
 	The_text.insert(END, stuff)
+	
 	#Close the file opened
 	text_file.close()
+
 
 #Save as the File 
 def save_as_file():
@@ -45,6 +66,66 @@ def save_as_file():
 
 		#Close The File
 		text_file.close()
+
+#Save file
+def save_file():
+	global open_status_name
+	if open_status_name:
+		#Save the File
+		text_file = open(open_status_name, 'w')
+		text_file.write(The_text.get(1.0, END))
+
+		#Close The File
+		text_file.close()
+		
+		#Status update
+		status_bar.config(text=f'Saved: {open_status_name}'        )
+	else:
+		save_as_file()
+
+#Cut Tool
+def cut_text(e):
+	global selected
+	#Check keyboard shortcut
+	if e:
+		selected = root.clipboard_get()
+	else:
+		if The_text.selection_get():
+			#Get selected text from the text box
+			selected = The_text.selection_get()
+			#deleted
+			The_text.delete("sel.first", "sel.last")
+			#CLear The board
+			root.clipboard_get()
+			root.clipboard_append(selected)
+
+
+#Copy Tool
+def copy_text(e):
+	global selected
+	#check keyboards shourtcut
+	if e:
+		selected = root.clipboard_get()
+	else:
+		if The_text.selection_get():
+			#Gets the text selected
+			selected = The_text.selection_get()
+			#CLear The board
+			root.clipboard_get()
+			root.clipboard_append(selected)
+
+#Paste Tool
+def paste_text(e):
+	global selected
+	#Check shortcut
+	if e:
+		selected = root.clipboard_get()
+	else:
+		if selected:
+			position = The_text.index(INSERT)
+			The_text.insert(position, selected)
+
+
 
 
 #Here I create a Main Frame
@@ -70,18 +151,18 @@ root.config(menu=myMenu)
 file_menu = Menu(myMenu)
 myMenu.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Open", command=open_file)
-file_menu.add_command(label="Save")
+file_menu.add_command(label="Save", command=save_file)
 file_menu.add_command(label="Save As", command=save_as_file)
 file_menu.add_command(label="New", command = new_file)
 file_menu.add_separator()
-file_menu.add_command(label="Exit")
+file_menu.add_command(label="Exit", command=root.quit)
 
 #Add an edit menu
 edit_menu = Menu(myMenu)
 myMenu.add_cascade(label="Edit", menu=edit_menu)
-edit_menu.add_command(label="Copy")
-edit_menu.add_command(label="Paste")
-edit_menu.add_command(label="Cut")
+edit_menu.add_command(label="Copy   Ctrl+x", command=lambda: copy_text(False))
+edit_menu.add_command(label="Paste   Ctrl+v", command=lambda: paste_text(False))
+edit_menu.add_command(label="Cut      Ctrl+x", command=lambda: cut_text(False))
 edit_menu.add_command(label="Undo")
 edit_menu.add_command(label="Redo")
 
@@ -89,7 +170,10 @@ edit_menu.add_command(label="Redo")
 status_bar = Label(root, text="Ready        ", anchor=E)
 status_bar.pack(fill=X, side=BOTTOM, ipady=5)
 
-
+#Edit Bindings 
+root.bind('<Control-Key-x>', cut_text)
+root.bind('<Control-Key-c>', copy_text)
+root.bind('<Control-Key-v>', paste_text)
 
 root.mainloop()
 
